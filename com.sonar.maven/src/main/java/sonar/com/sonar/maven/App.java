e76612b45ca70f0c62974a2c4514980a916f623b
Pablo Lopez <plopez@viewnext.com>
2019-02-11 10:16:51 +0100

    First commit

---
+++ com.sonar.maven/src/main/java/sonar/com/sonar/maven/App.java
@@ -0,0 +1,61 @@
+package sonar.com.sonar.maven;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        int i;
+        
+        for(i=0;i<10;i++)
+        {
+        	System.out.println("Hello World " +i);
+        }
+        
+        if(i==0)
+        {
+        	System.out.println("Hello World " +i);
+        	
+        	 if(i==0)
+             {
+             	System.out.println("Hello World " +i);
+             		
+             }
+        }
+        
+        List<Integer> lista = new ArrayList<Integer>();
+        Scanner reader = new Scanner(System.in);
+        int numero = 0;
+         
+        System.out.println("Introduce números. El cero para salir");
+        System.out.println("Introduce números. El cero para salir");
+        System.out.println("Introduce números. El cero para salir");
+        System.out.println("Introduce números. El cero para salir");
+        
+        
+         
+        do {			
+            numero = reader.nextInt();
+            lista.add(numero);
+        } while (numero!=0);
+        
+        String mivariable = "<undefined>";
+        
+        try {
+        	
+        	mivariable = mivariable + "aaaa";
+        	
+        }catch(Exception e){
+        	
+        }
+    }
+}
